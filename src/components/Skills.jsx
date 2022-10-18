538ad8c4319fb17003550df565941f8b0311968c
Robbe Van Herck <robbe@robbevanherck.be>
2022-10-18 18:51:39 +0200

    Forgot RDFa in skills
    (maybe that's why it's not in my skillset :thinking:)

--- src/components/Skills.jsx
+++ src/components/Skills.jsx
@@ -6,7 +6,7 @@ export default function Skills(props) {
   const skillUris = graph.each(user, CV('hasSkill'));
   const skillsByLevel = skillUris.reduce((acc, skillUri) => {
     const level = graph.any(skillUri, CV('skillLevel'));
-    const skillName = graph.any(skillUri, CV('skillName')).value;
+    const skillName = graph.any(skillUri, CV('skillName'))?.value;
 
     const key = (graph.match(skillUri, RDF('type'), CV('LanguageSkill')).length === 0)
         ? "programmingLanguages"
@@ -22,15 +22,22 @@ export default function Skills(props) {
   });
 
   return (
-    <section id="skills" class="half-block">
+    <section id="skills" className="half-block">
       <h2>Skills</h2>
       <h3>Programming Languages</h3>
       <ul>
         {Object.keys(skillsByLevel.programmingLanguages).length == 0 && (
           <li className="loading">Loading...</li>
         )}
-        {Object.keys(skillsByLevel.programmingLanguages).map(level => (
-          <li marker={level}>{skillsByLevel.programmingLanguages[level].join(", ")}</li>
+        {Object.keys(skillsByLevel.programmingLanguages).map((level, j) => (
+          <li marker={level} className="listify" key={j}>
+            {skillsByLevel.programmingLanguages[level].map((skill, i) => (
+              <span rel={CV("hasSkill").value} typeof={CV("Skill").value} key={i}>
+                <span property={CV("skillName").value}>{skill}</span>
+                <span property={CV("skillLevel").value} hidden>{level}</span>
+              </span>
+            ))}
+          </li>
         ))}
       </ul>
       <h3>Languages</h3>
@@ -38,8 +45,15 @@ export default function Skills(props) {
         {Object.keys(skillsByLevel.humanLanguages).length == 0 && (
           <li className="loading">Loading...</li>
         )}
-        {Object.keys(skillsByLevel.humanLanguages).map(level => (
-          <li marker={skillsByLevel.humanLanguages[level].join(", ")}>{level}</li>
+        {Object.keys(skillsByLevel.humanLanguages).map((level, i) => (
+          <li
+            marker={skillsByLevel.humanLanguages[level][0]}
+            rel={CV("hasSkill").value}
+            typeof={CV("LanguageSkill").value}
+            key={i}
+          >
+            {level}
+          </li>
         ))}
       </ul>
     </section>
