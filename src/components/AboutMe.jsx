f64ef755adeab4daf4927a99b7dcfba28e553cc9
Robbe Van Herck <robbe@robbevanherck.be>
2022-10-20 13:23:53 +0200

    Fetch images using authenticated fetch

--- src/components/AboutMe.jsx
+++ src/components/AboutMe.jsx
@@ -1,15 +1,40 @@
 import { FOAF, SCHEMA } from "../namespaces.js"
+import { useState } from "react";
+
+import { fetch as authFetch, getDefaultSession } from "@inrupt/solid-client-authn-browser";
 
 import ContactItems from "./ContactItems.jsx"
 
+
 export default function AboutMe(props) {
   const { user, graph } = props;
+
+  const [imageUri, setImageUri] = useState(null);
 
   const image = graph.anyValue(user, FOAF("img")) || graph.anyValue(user, SCHEMA("image"));
   let name = (
     graph.anyValue(user, FOAF("name")) ||
     graph.anyValue(user, SCHEMA("name"))
   );
+
+  if (image && !imageUri) {
+    console.log(getDefaultSession().info.isLoggedIn);
+    authFetch("https://solid.robbevanherck.be/robbevanherck/profile/card-private")
+      .then((response) => response.text())
+      .then(console.log);
+    authFetch(image)
+      .then(response => response.blob())
+      .then((data) => {
+        console.log(data);
+        return data;
+      })
+      .then(blob => URL.createObjectURL(blob))
+      .then(setImageUri)
+      .catch(err => {
+        console.log(err);
+        setImageUri(null);
+      });
+  }
 
   if (!name) {
     const firstName = (
@@ -32,7 +57,7 @@ export default function AboutMe(props) {
     <header id="about-me">
       <img
         id="profile-picture"
-        src={image || "https://via.placeholder.com/150x150"}
+        src={imageUri || "https://via.placeholder.com/150x150"}
         alt={name || "Profile picture"}
         property={image ? FOAF("img").value + " " + SCHEMA("image").value : undefined}
       />
